df8c4f723cf6630ea89595f0211a8ca0ce51be08
Jaymin <jaymin28panchal@gmail.com>
2026-08-22 00:36:09 +0530

    1st commit

---
+++ BeanCandy/Onboarding Files/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  BeanCandy
+//
+//  Created by Jayluu's Mac on 22/08/26.
+//
+
+import SwiftUI
+
+struct OnboardingView : View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
