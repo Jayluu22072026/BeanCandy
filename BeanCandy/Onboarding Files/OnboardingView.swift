fbd424c7dbb2d06c6150f8be3b60ae8a59c469d7
Jaymin <jaymin28panchal@gmail.com>
2026-08-22 00:57:37 +0530

    Test last check afetr adding to the repo

--- BeanCandy/Onboarding Files/OnboardingView.swift
+++ BeanCandy/Onboarding Files/OnboardingView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct OnboardingView : View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        ZStack {
+            Color.red
+        }
     }
 }
 
