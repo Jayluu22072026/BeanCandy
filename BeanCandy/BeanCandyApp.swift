52740010133e26b861450bce7c30da7de0f8ae70
Jaymin <jaymin28panchal@gmail.com>
2026-08-22 00:33:42 +0530

    Initial Commit

---
+++ BeanCandy/BeanCandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeanCandyApp.swift
+//  BeanCandy
+//
+//  Created by Jayluu's Mac on 22/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BeanCandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeanCandy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeanCandy
+//
+//  Created by Jayluu's Mac on 22/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
